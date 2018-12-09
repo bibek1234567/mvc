01496d45a9f8e08a42f2a8eb7bde4bd44ba6fad4
bibek sapkota <sapkotabibek97@gmail.com>
2018-12-09 19:25:50 +0545

    created namespace autoload, added functions in helpers/functions.php file and created Mysql class.

---
+++ system/DB/Mysql.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace System\DB;
+
+
+class Mysql
+{
+       protected $con = null;
+       protected $result = null;
+       protected $last_query = null;
+
+       public function __construct($host, $name, $user, $password=''){
+         $this->con = new \PDO("mysql:host='{$host}';dbname={$name}",$user,$pass);
+         $this->con->setAttribute(\PDO:ATTR_ERRMODE,\PDO::ERRMODE_EXCEPTION);
+     }
+
+     public function query($sql){
+         $this->last_query = $sql;
+         $this->result = $this->con->prepare($sql);
+         $this->result->execute();
+         return $this->result;
+     }
+
+     public function num_rows(){
+         return $this->result->rowCount();
+     }
+     public function fetch_assoc(){
+         return $this->result->fetchAll();
+     }
+
+     public function last_query(){
+         return $this->result->last_query;
+     }
+}
