1008a91679709e48203505ff80ab24f112ef7343
bibek sapkota <sapkotabibek97@gmail.com>
2018-12-13 19:33:31 +0545

    added delete and realted query builder functions

---
+++ apps/Models/Article.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Apps\Models;
+
+
+use System\Core\Models;
+
+class Article extends Models
+{
+   protected $table = 'articles';
+}
--- system/Core/Models.php
+++ system/Core/Models.php
@@ -14,6 +14,7 @@ abstract class Models extends Mysql
     protected $order = null;
     protected $offset = null;
     protected $limit = null;
+    protected $related = null;
 
     public function select($columns='*'){
         if(is_array($columns)){
@@ -63,16 +64,38 @@ abstract class Models extends Mysql
         $this->buildQuery('select');
         $this->query($this->sql);
         if ($this->num_rows()==1){
-            $data = $this->fetch_assoc()[0];
-            foreach ($data as $k => $v){
-                $this->{$k} = $v;
+            if(is_null($this->related)) {
+                $data = $this->fetch_assoc()[0];
+                foreach ($data as $k => $v) {
+                    $this->{$k} = $v;
+                }
+                     $this->reset();
+                return true;
+            }else{
+                $class = new $this->related['class_name'];
+                $data = $this->fetch_assoc()[0];
+                foreach ($data as $k => $v){
+                    $class->{$k} = $v;
+                }
+                $this->reset();
+                if($this->related['relation']=='parent') {
+                    $this->reset();
+                    return $class;
+                }else{
+                    $ret[]= $class;
+                    $this->reset();
+                    return $ret;
+                }
             }
-
             return true;
         }elseif($this->num_rows()>1){
             $data = $this->fetch_assoc();
             $ret = [];
-            $class_name = get_class($this);
+            if(is_null($this->related)) {
+                $class_name = get_class($this);
+            }else{
+                $class_name = $this->related['class_name'];
+            }
             foreach($data as $item){
                 $obj = new $class_name;
                 foreach ($item as $k => $v){
@@ -108,6 +131,17 @@ abstract class Models extends Mysql
         return true;
     }
 
+    public function delete(){
+        $this->buildQuery('delete');
+        $this->query($this->sql);
+        $this->reset();
+        $keys = $this->getDataColumns();
+        foreach($keys as $key){
+          unset($this->{$key});
+        }
+    return true;
+    }
+
     protected function reset(){
        $this->$sql = '';
        $this->$select = '*';
@@ -118,7 +152,18 @@ abstract class Models extends Mysql
     }
 
     protected function buildSelectQuery(){
-        $this->sql = "SELECT {$this->select} FROM {$this->table}";
+        if(is_null($this->realted)) {
+
+            $this->sql = "SELECT {$this->select} FROM {$this->table}";
+        }else{
+            $class = new $this->related['class_name'];
+            $this->sql = "SELECT {$this->select} FROM {$class->table}";
+            if($this->realted ['realtion']=='child'){
+                $this->where($this->realted['fk'], $this->{$this->pk});
+            }else{
+                $this->where($class->pk,$this->{$this->related['fk']});
+            }
+        }
         if (!is_null($this->conditions)){
             $this->sql.=" WHERE {$this->conditions}";
         }
@@ -159,20 +204,31 @@ abstract class Models extends Mysql
         $this->sql .=" WHERE{$this->pk} = '{$this->{$this->pk}}' ";
     }
 
+    protected function buildDeleteQuery()
+    {
+        $this->sql = "DELETE FROM {$this->table} WHERE {$this->pk}= {$this->{$this->pk}}";
 
-    protected function getDataColumns($type='keys'){
-        $all = get_class_vars(get_class($this));
-        $vars = get_object_vars($this);
 
-        $diff = array_diff($vars,$all);
-        if($type == 'data'){
-            return $diff;
-        }else{
-            return array_keys($diff);
+        protected
+        function getDataColumns($type = 'keys')
+        {
+            $all = get_class_vars(get_class($this));
+            $vars = get_object_vars($this);
+
+            $diff = array_diff($vars, $all);
+            if ($type == 'data') {
+                return $diff;
+            } else {
+                return array_keys($diff);
+            }
         }
+
     }
 
-
+    public  function related($class_name, $fk, $realtion){
+        $this->related = compact('class_name', 'fk','relation');
+        return $this;
+    }
 
     protected function buildQuery($mode){
         switch ($mode){
@@ -185,6 +241,10 @@ abstract class Models extends Mysql
 
             case 'update':
                 $this->buildUpdateQuery();
+                break;
+
+            case 'delete':
+                $this->buildDeleteQuery();
                 break;
 
             default:
